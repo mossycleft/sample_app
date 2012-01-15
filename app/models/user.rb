82321e140004538b6ac054d6b76026d25aacb610
Lewis Clayton <mail@l.ew.is>
2012-01-15 15:49:37 +0000

    User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
-class User < ActiveRecord::Base
-  attr_accessible :name, :email
-end
+
 
 # == Schema Information
 #
@@ -12,4 +10,21 @@ end
 #  created_at :datetime
 #  updated_at :datetime
 #
+
+class User < ActiveRecord::Base
+  attr_accessible :name, :email
+  
+  email_regex = /\A[\w+\-.]+@[a-z\d\.-]+\.[a-z]+\z/i
+  
+  validates :name,  :presence => true,  
+                    :length   => { :maximum => 50 } 
+  validates :email, :presence => true,
+                    :format   => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false }
+                    
+
+  
+end
+
+  
 
